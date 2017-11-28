d02d83167c8845f0b9dacaba795c34ce40a17dec
bcollins0905 <b.w.collinssf@gmail.com>
2017-11-27 18:15:26 -0800

    fixed port

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-var PORT = 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -25,8 +24,8 @@ var routes = require("./controllers/burgers_controllers.js");
 
 app.use("/", routes);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
 
 
